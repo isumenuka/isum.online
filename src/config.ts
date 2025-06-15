4aef72265e4d52ae6509588bcfaf4419b14031c0
ezsumm <105377034+isumenuka@users.noreply.github.com>
2025-06-15 19:13:02 +0530

    isumenuka 2.0
    Fix bugs

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const YOUTUBE_API_KEY = 'AIzaSyAjzyC6sbawTQQ8UzaDAYtavZUcQpZph-8';
+export const YOUTUBE_API_KEY = 'AIzaSyAn6rfVQMAeunFqgKUCwYlv1bxQ-x9WIJc';
 export const CHANNEL_HANDLE = '@ezsummextra';
 export const CHANNEL_ID = 'UCtBcHvEpqnFmwMVNZLmKOJQ';
