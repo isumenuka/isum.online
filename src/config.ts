f09adc6e41ea9ec7a0e75ef0850f73a37e4b0967
itz esuma <105377034+isumenuka@users.noreply.github.com>
2025-09-19 18:59:48 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY || '';
-export const CHANNEL_HANDLE = '@esumextra';
+export const CHANNEL_HANDLE = '@esumaextra';
 export const CHANNEL_ID = 'UCtBcHvEpqnFmwMVNZLmKOJQ';
