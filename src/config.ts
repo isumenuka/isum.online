438439261388ecd8fb559d1c96c90cbf757fb27c
itz esuma <105377034+isumenuka@users.noreply.github.com>
2025-09-19 18:57:47 +0530

    Update config.ts
    change handle

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const YOUTUBE_API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY || '';
-export const CHANNEL_HANDLE = '@ezsummextra';
+export const CHANNEL_HANDLE = '@esumextra';
 export const CHANNEL_ID = 'UCtBcHvEpqnFmwMVNZLmKOJQ';
